f8dcce69167a27d3516070539b497eb80d207ce9
Enzo Vezzaro <enzo-vezzaro@live.it>
2020-07-06 20:33:07 +0200

    Setup (Admin Framework & AWS Part 1)

--- app/index.js
+++ app/index.js
@@ -5,6 +5,10 @@ import { render } from 'react-dom';
 
 import App from './components/App';
 
+import Amplify from "aws-amplify";
+import awsmobile from "./aws-exports";
+Amplify.configure(awsmobile);
+
 render(
     <App />,
     document.querySelector('#root')
